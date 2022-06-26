d6524c5c09cf9cce4527925055f9f8a7021b70ef
Sietze van Buuren <s.van.buuren@gmail.com>
2022-06-26 20:31:56 +0200

    Use os.path in Sphinx conf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -13,9 +13,11 @@
 
 import os
 import sys
-import pathlib as pl
 import sphinx_immaterial
-sys.path.insert(0, pl.Path(__file__).parents[2].absolute())
+#import pathlib as pl
+#sys.path.insert(0, pl.Path(__file__).parents[2].absolute())
+path = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(path, '..', '..'))
 import mlpyqtgraph
 
 
