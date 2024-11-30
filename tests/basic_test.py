16f5a7b443997e927c91c5535491785beffa56e0
Sietze van Buuren <s.van.buuren@gmail.com>
2024-11-30 16:45:40 +0100

    test: Add test usage of decorator options
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -3,10 +3,22 @@
 import mlpyqtgraph as mpg
 
 
-def test_open_close():
+def test_simple_plot():
     """ Test opening and closing a figure """
 
-    @mpg.plotter()
+    @mpg.plotter
+    def main():
+        fig = mpg.figure(title='Test')
+        mpg.plot([1, 2, 3], [2, 3, 4])
+        fig.close()
+
+    main()
+
+
+def test_decorator_option():
+    """ Test usage of decorator options """
+
+    @mpg.plotter(line_color_profile='matplotlib')
     def main():
         fig = mpg.figure(title='Test')
         mpg.plot([1, 2, 3], [2, 3, 4])
