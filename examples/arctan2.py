9ca1d4ce8d15f44dc44c798ebd8bd4be953f96ef
Sietze van Buuren <s.van.buuren@gmail.com>
2025-12-31 16:56:26 +0100

    test: 3D axes aspect ratio and projection in examples
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- examples/arctan2.py
+++ examples/arctan2.py
@@ -12,7 +12,7 @@ def main():
     extent = 4
     nx = 48
     ny = 48
-    amplitude = 0.5
+    amplitude = 1.0
     x = np.linspace(-extent, extent, nx)
     y = np.linspace(-extent, extent, ny)
     z = np.zeros((nx, ny))
@@ -20,9 +20,11 @@ def main():
         z[i, :] = amplitude * np.arctan2(x, y[i])
 
     mpg.figure(title='arctan2(x, y)', layout_type='Qt')
-    mpg.surf(x, y, z, projection='orthographic')
+    mpg.surf(x, y, z)
     ax = mpg.gca()
+    ax.aspect_ratio = 'flat'
     ax.azimuth = 315
+    ax.projection = 'orthographic'
 
 if __name__ == '__main__':
     main()
