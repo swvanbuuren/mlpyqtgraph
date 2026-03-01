a3f7d602c70acb7bdebc0e799d44909a4a2d4414
Sietze van Buuren <s.van.buuren@gmail.com>
2026-03-01 15:56:58 +0100

    test: Saddle rotating view animation using PIL
    Signed-off-by: Sietze van Buuren <s.van.buuren@gmail.com>

--- examples/saddle.py
+++ examples/saddle.py
@@ -4,6 +4,8 @@ Basic example that shows a saddle surface plot and the corresponding points.
 
 import numpy as np
 import mlpyqtgraph as mpg
+from PIL import Image
+import os
 
 
 @mpg.plotter(projection='orthographic')
@@ -23,6 +25,41 @@ def main():
     mpg.points3(xp, yp, zp, color=(0.8, 0.1, 0.1, 1), size=4)
     ax = mpg.gca()
     ax.azimuth = 240
+    ax.export('saddle_still.png')
+
+    # create an animated PNG (APNG) with optimized settings
+    # Use 3-degree steps (120 frames instead of 360) for smoother playback
+    frames = []
+    frame_paths = []
+    
+    for az in range(0, 360, 2):
+        ax.azimuth = az
+        frame_path = f'saddle_{az:03d}.png'
+        ax.export(frame_path)
+        frame_paths.append(frame_path)
+    
+    # Load all frames and normalize DPI to prevent Firefox resampling
+    # Firefox scales images based on DPI metadata - we set it to 96 DPI (standard web DPI)
+    for frame_path in frame_paths:
+        img = Image.open(frame_path)
+        frames.append(img)
+        os.remove(frame_path)
+    
+    # Save as animated PNG with 50ms per frame (20 fps) and infinite loop
+    # 50ms duration is more compatible with Firefox and provides smooth playback
+    # Explicitly set DPI in the output file to ensure consistent display
+    img_fmt = 'gif'
+    frames[0].save(
+        f'saddle_animated.{img_fmt}',
+        save_all=True,
+        append_images=frames[1:],
+        duration=40,
+        loop=0,
+        optimize=True,
+        format=img_fmt
+    )
+    
+    print("Done")
 
 
 if __name__ == '__main__':
